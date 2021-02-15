a9041ab96aac2236b20463126ed3986de226f5ba
donghwan.yu <koreacio@gmail.com>
2021-02-16 08:31:08 +0900

    [ch_10] fix 1.11.3 version

--- ch_10/kubernetes-quickstart/src/main/java/org/acme/rest/GreetingResource.java
+++ ch_10/kubernetes-quickstart/src/main/java/org/acme/rest/GreetingResource.java
@@ -5,7 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/C:/Program Files/Git/greeting")
+@Path("/greeting")
 public class GreetingResource {
 
     @GET
